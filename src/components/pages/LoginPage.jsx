37613a3ada220caa0789f483795d498192123d84
Jerome <micl.jerome@gmail.com>
2023-03-17 22:02:19 +0100

    ✨ F01: LOGIN > Add title and form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -5,19 +5,31 @@ function LoginPage() {
   const [prenom, setPrenom] = useState("");
 
   // comportements
-  const handleClick = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
     alert(`Bonjour ${prenom}`);
     setPrenom("");
+  };
+
+  const handleChange = (event) => {
+    setPrenom(event.target.value);
   };
 
   // render
   return (
     <div>
       <h1>Bienvenue chez nous !</h1>
+      <br />
       <h2>Connectez-vous</h2>
-      <form>
-        <input required type="text" placeholder="Entrez votre prénom..." />
-        <button onClick={handleClick}>Accéder à votre espace</button>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          value={prenom}
+          onChange={handleChange}
+          type="text"
+          placeholder="Entrez votre prénom..."
+          required
+        />
+        <button>Accéder à votre espace</button>
       </form>
     </div>
   );
