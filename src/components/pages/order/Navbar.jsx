b72036534fce5551284d27b1e313f14654a0eaff
Jerome <micl.jerome@gmail.com>
2023-05-20 17:25:50 +0200

    ♻️ F04: ORDER > Navbar > Create NavbarRightSide

--- src/components/pages/order/Navbar.jsx
+++ src/components/pages/order/Navbar.jsx
@@ -1,18 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
+import NavbarRightSide from "./NavbarRightSide";
 
 export default function Navbar({ username }) {
   return (
     <NavbarStyled>
       <div className="left-side">Left</div>
-      <div className="right-side">
-        Right
-        <h1>Bonjour {username}</h1>
-        <Link to="/">
-          <button>Déconnexion</button>
-        </Link>
-      </div>
+      <NavbarRightSide username={username} />
     </NavbarStyled>
   );
 }
@@ -26,9 +21,5 @@ const NavbarStyled = styled.nav`
 
   .left-side {
     background: pink;
-  }
-
-  .right-side {
-    background: purple;
   }
 `;
---
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+export default function NavbarRightSide({ username }) {
+  return (
+    <NavbarRightSideStyled>
+      Right
+      <h1>Bonjour {username}</h1>
+      <Link to="/">
+        <button>Déconnexion</button>
+      </Link>
+    </NavbarRightSideStyled>
+  );
+}
+
+const NavbarRightSideStyled = styled.div`
+  background: purple;
+`;
