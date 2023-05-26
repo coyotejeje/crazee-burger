6c5928ed491ad69d2486fc222c5c0dc39d0165c4
Jerome <micl.jerome@gmail.com>
2023-05-26 17:35:44 +0200

    ✨ F04: ORDER > Navbar > Create Profile

--- src/components/pages/order/NavbarRightSide.jsx
+++ src/components/pages/order/NavbarRightSide.jsx
@@ -5,15 +5,28 @@ import styled from "styled-components";
 export default function NavbarRightSide({ username }) {
   return (
     <NavbarRightSideStyled>
-      Right
-      <h1>Bonjour {username}</h1>
-      <Link to="/">
-        <button>Déconnexion</button>
-      </Link>
+      {/* <div className="admin-button">Admin Button</div> */}
+      <div className="profile">
+        <p>Bonjour {username}</p>
+        <Link to="/">
+          <button>Déconnexion</button>
+        </Link>
+      </div>
+      <div className="picture"></div>
     </NavbarRightSideStyled>
   );
 }
 
 const NavbarRightSideStyled = styled.div`
   background: purple;
+  display: flex;
+  align-items: center;
+
+  /* .admin-button {
+    background: lightblue;
+  } */
+
+  .profile {
+    background: yellow;
+  }
 `;
