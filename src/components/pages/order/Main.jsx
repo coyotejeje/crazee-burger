63dd06afc5292203f6c8a0c8c37f4fde5e430b76
Jerome <micl.jerome@gmail.com>
2023-05-28 10:14:47 +0200

    🚧 F05: ORDER > MAIN > Add grid layout

--- src/components/pages/order/Main.jsx
+++ src/components/pages/order/Main.jsx
@@ -3,7 +3,12 @@ import styled from "styled-components";
 import { theme } from "../../../theme";
 
 export default function Main() {
-  return <MainStyled></MainStyled>;
+  return (
+    <MainStyled>
+      <div className="basket">Basket</div>
+      <div className="menu">Menu</div>
+    </MainStyled>
+  );
 }
 
 const MainStyled = styled.div`
@@ -12,4 +17,15 @@ const MainStyled = styled.div`
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+
+  display: grid;
+  grid-template-columns: 25% 1fr;
+
+  .basket {
+    background: pink;
+  }
+
+  .menu {
+    background: purple;
+  }
 `;
