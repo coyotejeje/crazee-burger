6c07170945e5f7c96bf078ca1ade848dc4f9b979
Jerome <micl.jerome@gmail.com>
2024-12-19 10:18:11 +0100

    pp > Add my firstname

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Hello Jerome</h1>
+      <h1>Salut Jerome</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
